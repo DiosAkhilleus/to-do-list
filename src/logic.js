2973dc5f663c999375352086d3472907d928562c
DiosAkhilleus <abertin1809@gmail.com>
2021-03-04 00:36:38 -0600

    made functions cleaner

--- src/logic.js
+++ src/logic.js
@@ -1,23 +1,12 @@
 const todoItem = (title, description, dueDate, priority) => {
 
-
     return {  title, description, dueDate, priority  };
 }; 
 
-const setTitle = (item, str) => {
-    item.title = str;
-}
+const setTitle =    (item, str) => {item.title = str};
+const setDescr =    (item, str) => {item.description = str};
+const setDate =     (item, str) => {item.dueDate = str};
+const setPriority = (item, str) => {item.priority = str};
 
-const setDescr = (item, str) => {
-    item.description = str;
-}
-
-const setDate = (item, str) => {
-    item.dueDate = str;
-}
-
-const setPriority = (item, level) => {
-    item.priority = level;
-}
 
 export {  todoItem, setTitle, setDescr, setDate, setPriority  };
