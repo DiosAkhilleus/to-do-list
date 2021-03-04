2a0cacacfb7f1123ebb189cca01f0add91173b00
DiosAkhilleus <abertin1809@gmail.com>
2021-03-04 00:27:17 -0600

    added functions to edit todoItems

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
-import {  todoItem  } from './logic';
+import {  todoItem, setTitle, setDescr, setDate, setPriority  } from './logic';
 
 //todo structure: title, description, dueDate, priority
 
 console.log('Webpack Loaded');
 
-
-
-
-
+let jeff = todoItem('jeff', 'getJeff', 'March 20', 'Low');
--- src/logic.js
+++ src/logic.js
@@ -1,12 +1,23 @@
 const todoItem = (title, description, dueDate, priority) => {
-    
-
-
-
 
 
     return {  title, description, dueDate, priority  };
 }; 
 
+const setTitle = (item, str) => {
+    item.title = str;
+}
 
-export {  todoItem  };
+const setDescr = (item, str) => {
+    item.description = str;
+}
+
+const setDate = (item, str) => {
+    item.dueDate = str;
+}
+
+const setPriority = (item, level) => {
+    item.priority = level;
+}
+
+export {  todoItem, setTitle, setDescr, setDate, setPriority  };
